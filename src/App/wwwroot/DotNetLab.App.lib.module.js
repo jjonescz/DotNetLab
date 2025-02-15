28aad5fc2c15e01dbb431174bf595373fd4c2b54
Jan Jones <janjones@microsoft.com>
2025-02-15 18:41:13 +0100

    Avoid a potential JS keyword clash

--- src/App/wwwroot/DotNetLab.App.lib.module.js
+++ src/App/wwwroot/DotNetLab.App.lib.module.js
@@ -1,5 +1,5 @@
 export async function afterStarted(blazor) {
-    const exports = await blazor.runtime.getAssemblyExports('DotNetLab.App.dll');
+    const dotNetExports = await blazor.runtime.getAssemblyExports('DotNetLab.App.dll');
 
     // Check whether service worker has an update available.
     (async () => {
@@ -13,12 +13,12 @@ export async function afterStarted(blazor) {
         }
     
         if (registration.waiting) {
-            exports.DotNetLab.Lab.UpdateInfo.UpdateAvailable();
+            dotNetExports.DotNetLab.Lab.UpdateInfo.UpdateAvailable();
             return;
         }
     
         registration.addEventListener('updatefound', () => {
-            exports.DotNetLab.Lab.UpdateInfo.UpdateAvailable();
+            dotNetExports.DotNetLab.Lab.UpdateInfo.UpdateAvailable();
             return;
         });
     })();
