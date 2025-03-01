2b5e16451fa7dc1dd39b2b88b5abee0605f2be3b
Jan Jones <janjones@microsoft.com>
2025-03-01 14:55:22 +0100

    Display only first line of commit message

--- src/App/Utils/StringUtil.cs
+++ src/App/Utils/StringUtil.cs
@@ -2,6 +2,16 @@
 
 internal static class StringUtil
 {
+    public static string GetFirstLine(this string text)
+    {
+        foreach (var line in text.AsSpan().EnumerateLines())
+        {
+            return line.ToString();
+        }
+
+        return text;
+    }
+
     public static string SeparateThousands(this int number)
     {
         return number.ToString("N0");
