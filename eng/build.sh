5eca077bf07e89a2ebac20d13eb692c97aee411c
Jan Jones <janjones@microsoft.com>
2025-03-29 13:10:15 +0100

    Simplify Worker reference

--- eng/build.sh
+++ eng/build.sh
@@ -5,6 +5,4 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh --jsonfile global.json --install-dir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet workload install wasm-tools wasm-experimental
-./dotnet/dotnet build src/Worker
-./dotnet/dotnet publish -p:CompressionEnabled=false -o output src/App
-cp output/wwwroot/_framework/icudt_*.dat output/wwwroot/worker/wwwroot/_framework/
+./dotnet/dotnet publish -o output src/App
