47f80747c68b1946a2a362cc276cb5b9377666d8
Jan Jones <janjones@microsoft.com>
2025-05-04 14:18:22 +0200

    Handle both razor file kind types properly

--- src/RazorAccess/RazorAccessors.cs
+++ src/RazorAccess/RazorAccessors.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Razor.Language.Intermediate;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Razor;
 using Microsoft.NET.Sdk.Razor.SourceGenerators;
+using System.Runtime.CompilerServices;
 
 namespace DotNetLab;
 
@@ -18,13 +19,28 @@ public static class RazorAccessors
         AdditionalText additionalText,
         string? cssScope)
     {
-        return (RazorProjectItem)Activator.CreateInstance(typeof(SourceGeneratorProjectItem),
+        var ctor = typeof(SourceGeneratorProjectItem).GetConstructors().First();
+        object? fileKind = ctor.GetParameters()[3].ParameterType.IsEnum
+            ? GetFileKindFromPath(additionalText.Path)
+            : null; // will be automatically determined from file path
+
+        return (RazorProjectItem)ctor.Invoke([
             /* basePath: */ basePath,
             /* filePath: */ filePath,
             /* relativePhysicalPath: */ relativePhysicalPath,
-            /* fileKind: */ null, // will be automatically determined from file path
+            /* fileKind: */ fileKind,
             /* additionalText: */ additionalText,
-            /* cssScope: */ cssScope)!;
+            /* cssScope: */ cssScope]);
+    }
+
+    /// <summary>
+    /// Wrapper to avoid <see cref="MissingMethodException"/>s in the caller during JITing
+    /// even though the method is not actually called.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static object GetFileKindFromPath(string filePath)
+    {
+        return FileKinds.GetFileKindFromPath(filePath);
     }
 
     public static string Serialize(this DocumentIntermediateNode node)
