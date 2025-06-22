a480b7a3cfe8f85aac74207486d64fc31fd35add
Jan Jones <janjones@microsoft.com>
2025-06-22 14:24:02 +0200

    Rename LanguageServices client to avoid ambiguity

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddFluentUIComponents();
 
 builder.Services.AddScoped<WorkerController>();
 builder.Services.AddScoped<BlazorMonacoInterop>();
-builder.Services.AddScoped<LanguageServices>();
+builder.Services.AddScoped<LanguageServicesClient>();
 builder.Services.AddScoped<InputOutputCache>();
 builder.Services.AddScoped<TemplateCache>();
 
