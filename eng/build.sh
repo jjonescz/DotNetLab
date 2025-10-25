47785c5c058a61ed2404ca23dcc7a9ecf8f5f4e3
Jan Jones <janjones@microsoft.com>
2025-10-25 20:32:55 +0200

    Dogfood .NET 10 RTM

--- eng/build.sh
+++ eng/build.sh
@@ -2,7 +2,7 @@
 set -eu
 curl -sSL https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh --jsonfile global.json --install-dir ./dotnet
-./dotnet/dotnet --version
-./dotnet/dotnet workload install wasm-tools wasm-experimental
-./dotnet/dotnet publish -o output src/App
+./dotnet-install.sh --jsonfile global.json --install-dir ./.dotnet
+./.dotnet/dotnet --version
+./.dotnet/dotnet workload install wasm-tools wasm-experimental
+./.dotnet/dotnet publish -o output src/App
