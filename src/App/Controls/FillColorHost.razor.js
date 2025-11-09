5959c5642945e8cee45d4745a16497e8194de482
Jan Jones <janjones@microsoft.com>
2025-11-09 16:48:59 +0100

    Detect theme changes in FillColorHost

--- src/App/Controls/FillColorHost.razor.js
+++ src/App/Controls/FillColorHost.razor.js
@@ -1,14 +1,40 @@
-﻿import * as fluent from "../_content/Microsoft.FluentUI.AspNetCore.Components/Microsoft.FluentUI.AspNetCore.Components.lib.module.js"
+﻿import * as fluent from '../_content/Microsoft.FluentUI.AspNetCore.Components/Microsoft.FluentUI.AspNetCore.Components.lib.module.js';
 
+/**
+ * @returns {(() => void) | null}
+ */
 export function setFillColor(element, color) {
     if (element instanceof HTMLElement) {
-        if (typeof color === "string" && color.length) {
+        if (typeof color === 'string' && color.length) {
             const resource = fluent[color];
             if (resource) {
                 fluent.fillColor.setValueFor(element, resource.getValueFor(element.parentElement));
-                return;
+
+                const theme = document.getElementsByTagName('fluent-design-theme').item(0);
+                if (theme)
+                {
+                    const themeListener = (event) => {
+                        if (event.detail.name === 'mode') {
+                            fluent.fillColor.setValueFor(element, resource.getValueFor(element.parentElement));
+                        }
+                    };
+                    theme.addEventListener('onchange', themeListener);
+                    return () => theme.removeEventListener('onchange', themeListener);
+                }
+
+                return null;
             }
         }
+
         fluent.fillColor.deleteValueFor(element);
     }
+
+    return null;
+}
+
+/**
+ * @param {() => void} fn
+ */
+export function dispose(fn) {
+    fn();
 }
