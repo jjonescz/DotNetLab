a1b50f64ea1257f851fc14b11ba0b766cb3b0ae3
Jan Jones <janjones@microsoft.com>
2025-12-26 14:40:54 +0100

    Avoid JSON errors due to required properties

--- src/WorkerApi/WorkerJsonContext.cs
+++ src/WorkerApi/WorkerJsonContext.cs
@@ -1,6 +1,8 @@
 using BlazorMonaco.Editor;
 using DotNetLab.Lab;
+using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 
 namespace DotNetLab;
 
@@ -14,4 +16,54 @@ namespace DotNetLab;
 [JsonSerializable(typeof(SdkInfo))]
 [JsonSerializable(typeof(ImmutableArray<MarkerData>))]
 [JsonSerializable(typeof(PingResult))]
-public sealed partial class WorkerJsonContext : JsonSerializerContext;
+[Obsolete($"Use {nameof(WorkerJsonContext)} instead.")]
+public partial class WorkerJsonContextBase : JsonSerializerContext
+{
+    protected internal static JsonSerializerOptions DefaultOptions => s_defaultOptions;
+}
+
+public sealed class WorkerJsonContext
+#pragma warning disable CS0618 // Type or member is obsolete
+    : WorkerJsonContextBase
+#pragma warning restore CS0618
+    , IJsonTypeInfoResolver
+{
+    public WorkerJsonContext() { }
+
+    public WorkerJsonContext(JsonSerializerOptions options) : base(options) { }
+
+    public new static WorkerJsonContext Default { get; } = new(new(DefaultOptions));
+
+    private static MethodInfo GetTypeInfoBaseMethod
+    {
+        get
+        {
+            return field ??= getNoCache();
+
+            static MethodInfo getNoCache()
+            {
+                var interfaceMap = typeof(WorkerJsonContext).BaseType!.GetInterfaceMap(typeof(IJsonTypeInfoResolver));
+                var interfaceMethod = typeof(IJsonTypeInfoResolver).GetMethod(nameof(IJsonTypeInfoResolver.GetTypeInfo))!;
+                var index = interfaceMap.InterfaceMethods.IndexOf(interfaceMethod);
+                return interfaceMap.TargetMethods[index];
+            }
+        }
+    }
+
+    JsonTypeInfo? IJsonTypeInfoResolver.GetTypeInfo(Type type, JsonSerializerOptions options)
+    {
+        var typeInfo = (JsonTypeInfo?)GetTypeInfoBaseMethod.Invoke(this, [type, options]);
+
+        if (typeInfo is null)
+        {
+            return null;
+        }
+
+        foreach (var prop in typeInfo.Properties)
+        {
+            prop.IsRequired = false;
+        }
+
+        return typeInfo;
+    }
+}
