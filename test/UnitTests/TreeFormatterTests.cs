21531bc138e373c8fdc6dfb4685dbe4e6bf7281c
Jan Jones <janjones@microsoft.com>
2026-02-23 21:24:39 +0100

    Add tree formatter test

---
+++ test/UnitTests/TreeFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+namespace DotNetLab;
+
+[TestClass]
+public sealed class TreeFormatterTests : VerifyBase
+{
+    private static string Format([StringSyntax("C#")] string code)
+    {
+        var tree = CSharpSyntaxTree.ParseText(code);
+        var compilation = CSharpCompilation.Create(
+            "Test",
+            [tree],
+            RefAssemblyMetadata.All);
+        var model = compilation.GetSemanticModel(tree);
+
+        var formatter = new TreeFormatter();
+        var result = formatter.Format(model, tree.GetRoot(), new()
+        {
+            ExcludeSymbols = true,
+            ExcludeOperations = true,
+        });
+
+        return result.Text;
+    }
+
+    [TestMethod]
+    public Task SkippedTrivia() => Verify(Format("var x = [1; 2; 3];"));
+}
---
+++ test/UnitTests/Utils/VerifyInit.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DotNetLab;
+
+internal static class VerifyInit
+{
+    [ModuleInitializer]
+    internal static void Init()
+    {
+        UseProjectRelativeDirectory("Snapshots");
+    }
+}
