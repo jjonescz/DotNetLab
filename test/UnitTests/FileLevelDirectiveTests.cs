936304db45fc8a497064e7117a726a21185fe668
Jan Jones <janjones@microsoft.com>
2026-03-06 17:35:57 -0800

    Add tests for .NET-version-dependent file-level directives

--- test/UnitTests/FileLevelDirectiveTests.cs
+++ test/UnitTests/FileLevelDirectiveTests.cs
@@ -1,4 +1,5 @@
 ﻿using AwesomeAssertions;
+using System.Runtime.InteropServices;
 
 namespace DotNetLab;
 
@@ -33,6 +34,29 @@ public sealed class FileLevelDirectiveTests
 
         var expectedSuggestedValues = actualSuggestedValues.Order();
 
+        actualSuggestedValues.Should().Equal(expectedSuggestedValues);
+    }
+
+    [TestMethod]
+    public void TargetFramework()
+    {
+        // The current target framework should be among the suggested values.
+        var actualSuggestedValues = FileLevelDirective.Property.Descriptor
+            .SuggestValues("TargetFramework", "")
+            .Select(f => f.Split('=')[0]);
+        var currentMajorVersion = RuntimeInformation.FrameworkDescription.Split(' ')[1].Split('.')[0];
+        actualSuggestedValues.Should().Contain($"net{currentMajorVersion}.0");
+    }
+
+    [TestMethod]
+    public void WarningLevel()
+    {
+        // Warning level should be from 0 to currrent .NET version. It should also suggest 9999 which is commonly used as the max value.
+        var actualSuggestedValues = FileLevelDirective.Property.Descriptor
+            .SuggestValues("WarningLevel", "")
+            .Select(f => f.Split('=')[0]);
+        var currentMajorVersion = int.Parse(RuntimeInformation.FrameworkDescription.Split(' ')[1].Split('.')[0]);
+        var expectedSuggestedValues = Enumerable.Range(0, currentMajorVersion + 1).Concat([9999]).Select(i => i.ToString());
         actualSuggestedValues.Should().Equal(expectedSuggestedValues);
     }
 }
